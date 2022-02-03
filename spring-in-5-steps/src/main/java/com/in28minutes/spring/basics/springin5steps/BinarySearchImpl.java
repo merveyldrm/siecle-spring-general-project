6a69727f5ef32c41cafbe70688455a22b79a7aad
Merve Yıldırım <73492584+merveyldrm@users.noreply.github.com>
2022-02-03 17:28:35 +0300

    +

---
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
@@ -0,0 +1,11 @@
+package com.in28minutes.spring.basics.springin5steps;
+
+public class BinarySearchImpl {
+	
+	public int binarySearch(int[] numbers, int numberToSearchFor) {
+		//sorting an array
+		//search the array
+		//return the result
+		return 3;
+	}
+}
